baa671c9019e4693c12696942dfe4fcbcab08bdc
ZerothAngel <zerothangel@tyrannyofheaven.org>
2015-07-20 22:19:48 -0700

    Allow a reference group/block for solar roller.

--- main/solarroller.cs
+++ main/solarroller.cs
@@ -1,10 +1,35 @@
 private readonly SolarGyroController solarGyroController = new SolarGyroController(GyroControl.Roll);
 
+private bool FirstRun = true;
+
+private Base6Directions.Direction ShipUp, ShipForward;
+
 void Main(string argument)
 {
+    if (FirstRun)
+    {
+        FirstRun = false;
+        // See if there's a reference group
+        var referenceGroup = ZALibrary.GetBlockGroupWithName(this, "SolarGyroReference");
+        var reference = referenceGroup.Blocks.Count > 0 ? referenceGroup.Blocks[0] : null;
+        if (reference != null)
+        {
+            ShipUp = reference.Orientation.TransformDirection(Base6Directions.Direction.Up);
+            ShipForward = reference.Orientation.TransformDirection(Base6Directions.Direction.Forward);
+        }
+        else
+        {
+            // Default to grid up/forward
+            ShipUp = Base6Directions.Direction.Up;
+            ShipForward = Base6Directions.Direction.Forward;
+        }
+    }
+
     ZALibrary.Ship ship = new ZALibrary.Ship(this);
 
-    solarGyroController.Run(this, ship, argument);
+    solarGyroController.Run(this, ship, argument,
+                            shipUp: ShipUp,
+                            shipForward: ShipForward);
 
     ZALibrary.KickLoopTimerBlock(this, argument);
 }
