6b02e32afc9ee3818086ff58dd929a7a2262d186
ZerothAngel <zerothangel@tyrannyofheaven.org>
2015-07-20 22:29:54 -0700

    Determine jump sled reference automatically.

--- main/jumpsledcontroller.cs
+++ main/jumpsledcontroller.cs
@@ -8,10 +8,31 @@ private readonly SafeMode safeMode = new SafeMode();
 
 void Main(string argument)
 {
+    Base6Directions.Direction shipUp, shipForward;
+
+    // Look for our ship controllers
+    var controllers = new List<IMyTerminalBlock>();
+    GridTerminalSystem.GetBlocksOfType<IMyShipController>(controllers, block => block.CubeGrid == Me.CubeGrid);
+    // The remote and cockpit should be oriented the same, so it doesn't matter which one we pick
+    var reference = controllers.Count > 0 ? controllers[0] : null;
+    if (reference != null)
+    {
+        shipUp = reference.Orientation.TransformDirection(Base6Directions.Direction.Up);
+        shipForward = reference.Orientation.TransformDirection(Base6Directions.Direction.Forward);
+    }
+    else
+    {
+        // Default to grid up/forward
+        shipUp = Base6Directions.Direction.Up;
+        shipForward = Base6Directions.Direction.Forward;
+    }
+
     ZALibrary.Ship ship = new ZALibrary.Ship(this);
 
     batteryManager.Run(this, ship, argument);
-    solarGyroController.Run(this, ship, argument);
+    solarGyroController.Run(this, ship, argument,
+                            shipUp: shipUp,
+                            shipForward: shipForward);
     safeMode.Run(this, ship, false);
 
     ZALibrary.KickLoopTimerBlock(this, argument);
