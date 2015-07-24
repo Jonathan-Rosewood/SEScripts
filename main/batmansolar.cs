0421a6e57d0ee814ed5936f553912c859320b563
ZerothAngel <zerothangel@tyrannyofheaven.org>
2015-07-24 12:46:20 -0700

    Add battery manager for solar powered ships for public consumption.

---
+++ main/batmansolar.cs
@@ -0,0 +1,23 @@
+public class TimerBlockPowerDrainHandler : BatteryManager.PowerDrainHandler
+{
+    public void PowerDrainStarted(ZALibrary.Ship ship)
+    {
+        ZALibrary.StartTimerBlockWithName(ship.Blocks, POWER_DRAIN_START_NAME);
+    }
+
+    public void PowerDrainEnded(ZALibrary.Ship ship)
+    {
+        ZALibrary.StartTimerBlockWithName(ship.Blocks, POWER_DRAIN_END_NAME);
+    }
+}
+
+private readonly BatteryManager batteryManager = new BatteryManager(new TimerBlockPowerDrainHandler());
+
+void Main(string argument)
+{
+    ZALibrary.Ship ship = new ZALibrary.Ship(this);
+
+    batteryManager.Run(this, ship, argument);
+
+    ZALibrary.KickLoopTimerBlock(this, argument);
+}
---
+++ specs/batterymanager.spec
@@ -0,0 +1,3 @@
+[script]
+modules=batmansolar, batterymanager, ZALibrary
+out=out/Battery Manager/Script.cs
