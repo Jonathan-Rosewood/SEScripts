ac8db1637edab972eba2127ca3be02efa719e954
ZerothAngel <zerothangel@tyrannyofheaven.org>
2015-07-30 23:12:36 -0700

    Don't hold references to blocks, directly or indirectly.

--- main/minercontroller.cs
+++ main/minercontroller.cs
@@ -7,13 +7,10 @@ private const uint FramesPerRun = 5;
 private const double RunsPerSecond = 60.0 / FramesPerRun;
 
 private readonly Velocimeter velocimeter = new Velocimeter(10);
-private readonly GyroControl gyroControl = new GyroControl();
-private readonly ThrustControl thrustControl = new ThrustControl();
 private readonly PIDController thrustPID = new PIDController(1.0 / RunsPerSecond);
 
 private bool FirstRun = true;
 private bool Mining = false;
-private IMyCubeBlock Reference;
 
 private const double ThrustKp = 10000.0;
 private const double ThrustKi = 100.0;
@@ -25,17 +22,6 @@ void Main(string argument)
     {
         FirstRun = false;
 
-        var referenceGroup = ZALibrary.GetBlockGroupWithName(this, MINER_REFERENCE_GROUP);
-        if (referenceGroup == null)
-        {
-            throw new Exception("Missing group: " + MINER_REFERENCE_GROUP);
-        }
-        Reference = referenceGroup.Blocks[0];
-        var shipUp = Reference.Orientation.TransformDirection(Base6Directions.Direction.Up);
-        var shipForward = Reference.Orientation.TransformDirection(Base6Directions.Direction.Forward);
-        gyroControl.Init(this, shipUp: shipUp, shipForward: shipForward);
-        thrustControl.Init(this, shipUp: shipUp, shipForward: shipForward);
-
         thrustPID.Kp = ThrustKp;
         thrustPID.Ki = ThrustKi;
         thrustPID.Kd = ThrustKd;
@@ -44,33 +30,38 @@ void Main(string argument)
     }
 
     argument = argument.Trim().ToLower();
-    switch (argument)
+    if (argument.Length > 0)
     {
-        case "start":
-            gyroControl.EnableOverride(true);
-            thrustControl.Reset();
-            thrustControl.SetOverride(Base6Directions.Direction.Forward); // Get things moving
-            thrustPID.Reset();
-            velocimeter.Reset();
+        Base6Directions.Direction shipUp, shipForward;
+        GetReference(out shipUp, out shipForward);
+        var gyroControl = GetGyroControl(shipUp: shipUp, shipForward: shipForward);
+        var thrustControl = GetThrustControl(shipUp: shipUp, shipForward: shipForward);
 
-            if (!Mining)
-            {
-                Mining = true;
-                eventDriver.Schedule(1, Mine);
-            }
-            break;
-        case "stop":
-            Mining = false;
-            gyroControl.EnableOverride(false);
-            thrustControl.Reset();
-            break;
-        default:
-            if (argument.Length > 0)
-            {
+        switch (argument)
+        {
+            case "start":
+                gyroControl.EnableOverride(true);
+                thrustControl.Reset();
+                thrustControl.SetOverride(Base6Directions.Direction.Forward); // Get things moving
+                thrustPID.Reset();
+                velocimeter.Reset();
+
+                if (!Mining)
+                {
+                    Mining = true;
+                    eventDriver.Schedule(1, Mine);
+                }
+                break;
+            case "stop":
+                Mining = false;
+                gyroControl.EnableOverride(false);
+                thrustControl.Reset();
+                break;
+            default:
                 var ship = new ZALibrary.Ship(this);
                 dockingManager.HandleCommand(this, ship, argument);
-            }
-            break;
+                break;
+        }
     }
 
     eventDriver.Tick(this);
@@ -94,7 +85,9 @@ public void Mine(MyGridProgram program, EventDriver eventDriver)
 {
     if (!Mining) return;
 
-    velocimeter.TakeSample(Reference.GetPosition(), eventDriver.TimeSinceStart);
+    Base6Directions.Direction shipUp, shipForward;
+    var reference = GetReference(out shipUp, out shipForward);
+    velocimeter.TakeSample(reference.GetPosition(), eventDriver.TimeSinceStart);
 
     // Determine velocity
     var velocity = velocimeter.GetAverageVelocity();
@@ -110,6 +103,8 @@ public void Mine(MyGridProgram program, EventDriver eventDriver)
         // program.Echo(string.Format("Error: {0:F2}", error));
         // program.Echo(string.Format("Force: {0:F1} N", force));
         program.Echo("Mining");
+
+        var thrustControl = GetThrustControl(shipUp: shipUp, shipForward: shipForward);
         if (force > 0.0)
         {
             // Thrust forward
@@ -124,4 +119,33 @@ public void Mine(MyGridProgram program, EventDriver eventDriver)
     }
 
     eventDriver.Schedule(FramesPerRun, Mine);
+}
+
+private IMyCubeBlock GetReference(out Base6Directions.Direction shipUp, out Base6Directions.Direction shipForward)
+{
+    var referenceGroup = ZALibrary.GetBlockGroupWithName(this, MINER_REFERENCE_GROUP);
+    if (referenceGroup == null)
+    {
+        throw new Exception("Missing group: " + MINER_REFERENCE_GROUP);
+    }
+    var reference = referenceGroup.Blocks[0];
+
+    shipUp = reference.Orientation.TransformDirection(Base6Directions.Direction.Up);
+    shipForward = reference.Orientation.TransformDirection(Base6Directions.Direction.Forward);
+
+    return reference;
+}
+
+private GyroControl GetGyroControl(Base6Directions.Direction shipUp, Base6Directions.Direction shipForward)
+{
+    var gyroControl = new GyroControl();
+    gyroControl.Init(this, shipUp: shipUp, shipForward: shipForward);
+    return gyroControl;
+}
+
+private ThrustControl GetThrustControl(Base6Directions.Direction shipUp, Base6Directions.Direction shipForward)
+{
+    var thrustControl = new ThrustControl();
+    thrustControl.Init(this, shipUp: shipUp, shipForward: shipForward);
+    return thrustControl;
 }
