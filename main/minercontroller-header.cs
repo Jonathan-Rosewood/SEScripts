7f2b3b901d53471429c9f8999e80c8ee6481e905
ZerothAngel <zerothangel@tyrannyofheaven.org>
2015-08-01 00:46:43 -0700

    Mining speed of 1 m/s actually works fine.

--- main/minercontroller-header.cs
+++ main/minercontroller-header.cs
@@ -5,6 +5,6 @@ const string MINER_REFERENCE_GROUP = "*Simon RC*";
 // block and do nothing else (not even start itself)
 const string MINER_CLOCK_GROUP = "Simon Clock";
 
-const double TARGET_MINING_SPEED = 0.5; // In meters per second
+const double TARGET_MINING_SPEED = 1.0; // In meters per second
 
 const bool ABANDONMENT_ENABLED = true; // Set to false to disable abandonment check
