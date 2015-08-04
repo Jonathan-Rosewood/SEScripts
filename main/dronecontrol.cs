0e874ef1bc935271571a1567bb5d18102e8087d3
ZerothAngel <zerothangel@tyrannyofheaven.org>
2015-08-04 15:11:23 -0700

    Unknowingly broke Drone Controller with change to EventDriver's main handling.

--- main/dronecontrol.cs
+++ main/dronecontrol.cs
@@ -1,8 +1,8 @@
-private readonly EventDriver eventDriver = new EventDriver(timerName: STANDARD_LOOP_TIMER_BLOCK_NAME);
-private readonly DockingManager dockingManager = new DockingManager();
-private readonly SafeMode safeMode = new SafeMode();
-private readonly BatteryMonitor batteryMonitor = new BatteryMonitor();
-private readonly SolarRotorController rotorController = new SolarRotorController();
+public readonly EventDriver eventDriver = new EventDriver(timerName: STANDARD_LOOP_TIMER_BLOCK_NAME);
+public readonly DockingManager dockingManager = new DockingManager();
+public readonly SafeMode safeMode = new SafeMode();
+public readonly BatteryMonitor batteryMonitor = new BatteryMonitor();
+public readonly SolarRotorController rotorController = new SolarRotorController();
 
 private bool FirstRun = true;
 
@@ -18,17 +18,16 @@ void Main(string argument)
 
     dockingManager.HandleCommand(commons, argument);
 
-    if (eventDriver.Tick(commons))
-    {
-        // This really seems like it should be determined once per run
-        var isConnected = ZACommons.IsConnectedAnywhere(commons.Blocks);
+    eventDriver.Tick(commons, () =>
+            {
+                // This really seems like it should be determined once per run
+                var isConnected = ZACommons.IsConnectedAnywhere(commons.Blocks);
 
-        dockingManager.Run(commons, isConnected);
-        safeMode.Run(commons, isConnected);
-        batteryMonitor.Run(commons, isConnected);
-        if (MAX_POWER_ENABLED) rotorController.Run(commons);
+                dockingManager.Run(commons, isConnected);
+                safeMode.Run(commons, isConnected);
+                batteryMonitor.Run(commons, isConnected);
+                if (MAX_POWER_ENABLED) rotorController.Run(commons);
 
-        eventDriver.Schedule(1.0);
-        eventDriver.KickTimer(commons);
-    }
+                eventDriver.Schedule(1.0);
+            });
 }
