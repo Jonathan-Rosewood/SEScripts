8b93b34bbf7bbd8d5048b549684217133b62162c
ZerothAngel <zerothangel@tyrannyofheaven.org>
2015-08-06 12:01:58 -0700

    Increase drone controller autopilot buffer & distance to 5 meters.

--- main/dronecontrol-header.cs
+++ main/dronecontrol-header.cs
@@ -35,5 +35,5 @@ const bool MAX_POWER_ENABLED = false; // Set to true to enable rotor code
 // SmartUndock (ship-dependent)
 const double SMART_UNDOCK_RTB_SPEED = 25.0; // In meters per second
 const double AUTOPILOT_MIN_SPEED = 1.0; // In meters per second
-const double AUTOPILOT_TTT_BUFFER = 2.5; // Time-to-target buffer, in seconds
-const double AUTOPILOT_DISENGAGE_DISTANCE = 2.5; // In meters
+const double AUTOPILOT_TTT_BUFFER = 5.0; // Time-to-target buffer, in seconds
+const double AUTOPILOT_DISENGAGE_DISTANCE = 5.0; // In meters
