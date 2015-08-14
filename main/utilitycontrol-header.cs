851a45a73f6d2713900f155302c5a8ea701f52c2
ZerothAngel <zerothangel@tyrannyofheaven.org>
2015-08-14 13:05:44 -0700

    Update smart undock values for utility controller.

--- main/utilitycontrol-header.cs
+++ main/utilitycontrol-header.cs
@@ -3,7 +3,7 @@ const bool ABANDONMENT_ENABLED = true; // Set to false to disable abandonment ch
 const bool MAX_POWER_ENABLED = false; // Set to true to enable rotor code
 
 // SmartUndock (ship-dependent)
-const double SMART_UNDOCK_RTB_SPEED = 10.0; // In meters per second
+const double SMART_UNDOCK_RTB_SPEED = 25.0; // In meters per second
 const double AUTOPILOT_MIN_SPEED = 1.0; // In meters per second
-const double AUTOPILOT_TTT_BUFFER = 10.0; // Time-to-target buffer, in seconds
-const double AUTOPILOT_DISENGAGE_DISTANCE = 10.0; // In meters
+const double AUTOPILOT_TTT_BUFFER = 5.0; // Time-to-target buffer, in seconds
+const double AUTOPILOT_DISENGAGE_DISTANCE = 5.0; // In meters
