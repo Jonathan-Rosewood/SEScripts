b7c3682d394dc97563002ca8f374b641ee9c52b2
ZerothAngel <zerothangel@tyrannyofheaven.org>
2015-08-14 12:28:23 -0700

    Increase undock autopilot delay to 3 seconds.

--- utility/smartundock.cs
+++ utility/smartundock.cs
@@ -41,7 +41,7 @@ public class SmartUndock
                 // Schedule the autopilot
                 autopilot.Init(commons, eventDriver, (Vector3D)UndockTarget,
                                SMART_UNDOCK_UNDOCK_SPEED,
-                               delay: 2.0);
+                               delay: 3.0);
             }
 
             // Next, physically undock
