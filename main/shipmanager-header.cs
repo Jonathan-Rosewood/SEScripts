ef3d3df827bbcdf5fbb85a3835497950a3e50313
ZerothAngel <zerothangel@tyrannyofheaven.org>
2016-03-26 17:41:03 -0700

    Ship Manager script: Remove dead option for PowerManager.

--- main/shipmanager-header.cs
+++ main/shipmanager-header.cs
@@ -6,7 +6,6 @@ const bool SIMPLE_AIRLOCK_ENABLE = true;
 const bool COMPLEX_AIRLOCK_ENABLE = true;
 const bool OXYGEN_MANAGER_ENABLE = true;
 const bool AIR_VENT_MANAGER_ENABLE = true;
-const bool POWER_MANAGER_ENABLE = true;
 const bool REFINERY_MANAGER_ENABLE = true;
 const bool PRODUCTION_MANAGER_ENABLE = false;
 const bool TIMER_KICKER_ENABLE = true;
