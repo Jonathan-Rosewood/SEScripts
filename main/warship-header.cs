273f637a7806e2b576297303ea4d1b20aced805b
ZerothAngel <zerothangel@tyrannyofheaven.org>
2016-04-03 10:19:26 -0700

    Create Warship script, a stripped down version of the Survival Ship script (for now).

---
+++ main/warship-header.cs
@@ -0,0 +1,3 @@
+const bool ABANDONMENT_ENABLED = false;
+
+const bool CONTROL_CHECK_ENABLED = true;
---
+++ main/warship.cs
@@ -0,0 +1,65 @@
+//! Warship Manager
+//@ shipcontrol eventdriver doorautocloser simpleairlock oxygenmanager
+//@ redundancy damagecontrol safemode cruisecontrol emergencystop
+public class MySafeModeHandler : SafeModeHandler
+{
+    public void SafeMode(ZACommons commons, EventDriver eventDriver)
+    {
+        // Check after 1 second (let timer block's action take effect)
+        eventDriver.Schedule(1.0, (c,ed) =>
+                {
+                    new EmergencyStop().SafeMode(c, ed);
+                });
+    }
+}
+
+private readonly EventDriver eventDriver = new EventDriver(timerName: STANDARD_LOOP_TIMER_BLOCK_NAME);
+private readonly DoorAutoCloser doorAutoCloser = new DoorAutoCloser();
+private readonly SimpleAirlock simpleAirlock = new SimpleAirlock();
+private readonly OxygenManager oxygenManager = new OxygenManager();
+private readonly RedundancyManager redundancyManager = new RedundancyManager();
+private readonly DamageControl damageControl = new DamageControl();
+private readonly SafeMode safeMode = new SafeMode(new MySafeModeHandler());
+private readonly CruiseControl cruiseControl = new CruiseControl();
+private readonly ZAStorage myStorage = new ZAStorage();
+
+private readonly ShipOrientation shipOrientation = new ShipOrientation();
+
+private bool FirstRun = true;
+
+void Main(string argument)
+{
+    var commons = new ShipControlCommons(this, shipOrientation,
+                                         storage: myStorage);
+
+    if (FirstRun)
+    {
+        FirstRun = false;
+
+        myStorage.Decode(Storage);
+
+        shipOrientation.SetShipReference(commons, "CruiseControlReference");
+
+        doorAutoCloser.Init(commons, eventDriver);
+        simpleAirlock.Init(commons, eventDriver);
+        oxygenManager.Init(commons, eventDriver);
+        redundancyManager.Init(commons, eventDriver);
+        safeMode.Init(commons, eventDriver);
+        cruiseControl.Init(commons, eventDriver, LivenessCheck);
+    }
+
+    eventDriver.Tick(commons, preAction: () => {
+            damageControl.HandleCommand(commons, eventDriver, argument);
+            safeMode.HandleCommand(commons, eventDriver, argument);
+            cruiseControl.HandleCommand(commons, eventDriver, argument);
+        });
+
+    if (commons.IsDirty) Storage = myStorage.Encode();
+}
+
+
+bool LivenessCheck(ZACommons commons, EventDriver eventDriver)
+{
+    if (CONTROL_CHECK_ENABLED) safeMode.TriggerIfUncontrolled(commons, eventDriver);
+    return !safeMode.Abandoned;
+}
