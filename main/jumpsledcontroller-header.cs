d3597bc697baa8ba174fc94809f1828bf540c085
ZerothAngel <zerothangel@tyrannyofheaven.org>
2016-04-08 00:01:24 -0700

    Add a few more modules to Jump Sled script.

--- main/jumpsledcontroller-header.cs
+++ main/jumpsledcontroller-header.cs
@@ -1,4 +1,1 @@
-const string BATTERY_MANAGER_DISCHARGE_INTERVAL = "00:00:25"; // HH:MM[:SS]
-const string BATTERY_MANAGER_RECHARGE_INTERVAL = "00:00:05"; // HH:MM[:SS]
-
 const bool ABANDONMENT_ENABLED = false; // Meant to be abandoned!
--- main/jumpsledcontroller.cs
+++ main/jumpsledcontroller.cs
@@ -1,16 +1,34 @@
 //! Jump Sled Controller
-//@ shipcontrol eventdriver safemode batterymonitor reactormanager
-//@ solargyrocontroller
+//@ shipcontrol eventdriver batterymonitor oxygenmanager
+//@ timerkicker reactormanager damagecontrol
+//@ safemode cruisecontrol solargyrocontroller emergencystop
+public class MySafeModeHandler : SafeModeHandler
+{
+    public void SafeMode(ZACommons commons, EventDriver eventDriver)
+    {
+        // Check after 1 second (let timer block's action take effect)
+        eventDriver.Schedule(1.0, (c,ed) =>
+                {
+                    new EmergencyStop().SafeMode(c, ed);
+                });
+    }
+}
+
 private readonly EventDriver eventDriver = new EventDriver(timerName: STANDARD_LOOP_TIMER_BLOCK_NAME);
-private readonly SafeMode safeMode = new SafeMode();
 private readonly BatteryMonitor batteryMonitor = new BatteryMonitor();
+private readonly OxygenManager oxygenManager = new OxygenManager();
+private readonly TimerKicker timerKicker = new TimerKicker();
 private readonly ReactorManager reactorManager = new ReactorManager();
+private readonly DamageControl damageControl = new DamageControl();
+private readonly SafeMode safeMode = new SafeMode();
+private readonly CruiseControl cruiseControl = new CruiseControl();
 private readonly SolarGyroController solarGyroController =
     new SolarGyroController(
                             //GyroControl.Yaw,
                             GyroControl.Pitch,
                             GyroControl.Roll
                             );
+private readonly ZAStorage myStorage = new ZAStorage();
 
 private readonly ShipOrientation shipOrientation = new ShipOrientation();
 
@@ -24,17 +42,35 @@ void Main(string argument)
     {
         FirstRun = false;
 
+        myStorage.Decode(Storage);
+
         shipOrientation.SetShipReference<IMyShipController>(commons.Blocks);
 
-        safeMode.Init(commons, eventDriver);
         batteryMonitor.Init(commons, eventDriver);
+        oxygenManager.Init(commons, eventDriver);
+        timerKicker.Init(commons, eventDriver);
         reactorManager.Init(commons, eventDriver);
-        solarGyroController.Init(commons, eventDriver);
+
+        safeMode.Init(commons, eventDriver);
+        cruiseControl.Init(commons, eventDriver, LivenessCheck);
+        solarGyroController.ConditionalInit(commons, eventDriver, true);
     }
 
     eventDriver.Tick(commons, preAction: () =>
             {
                 reactorManager.HandleCommand(commons, eventDriver, argument);
+                damageControl.HandleCommand(commons, eventDriver, argument);
+                safeMode.HandleCommand(commons, eventDriver, argument);
+                cruiseControl.HandleCommand(commons, eventDriver, argument);
                 solarGyroController.HandleCommand(commons, eventDriver, argument);
             });
+
+    if (commons.IsDirty) Storage = myStorage.Encode();
+}
+
+
+bool LivenessCheck(ZACommons commons, EventDriver eventDriver)
+{
+    safeMode.TriggerIfUncontrolled(commons, eventDriver);
+    return !safeMode.Abandoned;
 }
