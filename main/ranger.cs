f05b9baaba8bb05a22356e8d507469af3bac432f
ZerothAngel <zerothangel@tyrannyofheaven.org>
2016-12-12 13:12:33 -0800

    ranger: Add target distance to text panel output.

--- main/ranger.cs
+++ main/ranger.cs
@@ -2,27 +2,29 @@
 //@ commons rangefinder
 public void TargetAction(ZACommons commons, Vector3D target)
 {
-    var targetGroup = commons.GetBlockGroupWithName(RANGEFINDER_TARGET_GROUP);
-    if (targetGroup != null)
-    {
-        var targetString = string.Format(RANGEFINDER_TARGET_FORMAT,
-                                         target.GetDim(0),
-                                         target.GetDim(1),
-                                         target.GetDim(2));
-
-        foreach (var panel in ZACommons.GetBlocksOfType<IMyTextPanel>(targetGroup.Blocks))
-        {
-            panel.WritePublicText(targetString);
-        }
-    }
-
-    // Also output to terminal
+    // Output to terminal
     commons.Echo(string.Format("Target: {0:F2}, {1:F2}, {2:F2}",
                                target.GetDim(0),
                                target.GetDim(1),
                                target.GetDim(2)));
     var distance = (target - commons.Me.GetPosition()).Length();
     commons.Echo(string.Format("Distance: {0:F2} m", distance));
+
+    // Also to text panel(s)
+    var targetGroup = commons.GetBlockGroupWithName(RANGEFINDER_TARGET_GROUP);
+    if (targetGroup != null)
+    {
+        var targetString = string.Format(RANGEFINDER_TARGET_FORMAT,
+                                         target.GetDim(0),
+                                         target.GetDim(1),
+                                         target.GetDim(2),
+                                         distance);
+
+        foreach (var panel in ZACommons.GetBlocksOfType<IMyTextPanel>(targetGroup.Blocks))
+        {
+            panel.WritePublicText(targetString);
+        }
+    }
 }
 
 private Rangefinder.LineSample first, second;
