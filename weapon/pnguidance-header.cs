8cb47abb2f8d1647087004d466baee3558360e83
ZerothAngel <zerothangel@tyrannyofheaven.org>
2017-11-21 12:56:25 -0800

    pnguidance: Rework post-PN stuff into something aim point-based.

--- weapon/pnguidance-header.cs
+++ weapon/pnguidance-header.cs
@@ -1,3 +1,2 @@
 // ProNavGuidance
-const double PN_GUIDANCE_GAIN = 3.0;
-const float PN_MAX_GYRO_RPM = 30.0f;
+const double PN_GUIDANCE_GAIN = 5.0;
--- weapon/pnguidance.cs
+++ weapon/pnguidance.cs
@@ -1,11 +1,31 @@
-//@ shipcontrol eventdriver basemissileguidance
+//@ shipcontrol eventdriver basemissileguidance seeker
 public class ProNavGuidance : BaseMissileGuidance
 {
     private const uint FramesPerRun = 1;
+    private const double RunsPerSecond = 60.0 / FramesPerRun;
+
+    private readonly Seeker seeker = new Seeker(1.0 / RunsPerSecond);
+
+    private double ForwardAcceleration;
 
     public void Init(ZACommons commons, EventDriver eventDriver)
     {
         var shipControl = (ShipControlCommons)commons;
+
+        if (shipControl.ShipController == null)
+        {
+            throw new Exception("No ship controller on board?");
+        }
+
+        // Figure out max forward acceleration, a=F/m
+        float maxForce = 0.0f;
+        var thrusterList = shipControl.ThrustControl.GetThrusters(Base6Directions.Direction.Forward);
+        thrusterList.ForEach(thruster => maxForce += thruster.GetMaximum<float>("Override"));
+        ForwardAcceleration = 1000.0 * maxForce / shipControl.ShipController.CalculateShipMass().PhysicalMass;
+
+        seeker.Init(shipControl,
+                    shipUp: shipControl.ShipUp,
+                    shipForward: shipControl.ShipForward);
 
         // Full burn
         var thrustControl = shipControl.ThrustControl;
@@ -17,22 +37,12 @@ public class ProNavGuidance : BaseMissileGuidance
         thrustControl.Enable(Base6Directions.Direction.Left, false);
         thrustControl.Enable(Base6Directions.Direction.Right, false);
 
-        // Reset gyro
-        var gyroControl = shipControl.GyroControl;
-        gyroControl.Reset();
-
         eventDriver.Schedule(0, Run);
-    }
-
-    private float ClampRPM(float value)
-    {
-        return Math.Max(-PN_MAX_GYRO_RPM, Math.Min(PN_MAX_GYRO_RPM, value));
     }
 
     public void Run(ZACommons commons, EventDriver eventDriver)
     {
         var shipControl = (ShipControlCommons)commons;
-        var gyroControl = shipControl.GyroControl;
 
         Vector3D? velocity = shipControl.LinearVelocity;
         if (velocity != null)
@@ -48,21 +58,21 @@ public class ProNavGuidance : BaseMissileGuidance
             var direction = Vector3D.Normalize((Vector3D)velocity);
             var accel = Vector3D.Cross(direction * -PN_GUIDANCE_GAIN * relativeVelocity.Length(), omega);
 
-            // Translate into local space
-            var toLocal = MatrixD.Invert(MatrixD.CreateWorld(Vector3D.Zero, shipControl.ReferenceForward, shipControl.ReferenceUp));
-            var localAccel = Vector3D.Transform(-accel, toLocal);
+            // Translate acceleration to an aim point
+            // Basically, we know 2 sides of a right triangle:
+            // ForwardAcceleration (hyp) and accel. The third side is coincident
+            // with velocity. This is what we're after (vDistance).
+            var vDistance = Math.Sqrt(Math.Max(ForwardAcceleration * ForwardAcceleration -
+                                               Vector3D.Dot(accel, accel), 0.0));
+            var aimPoint = Vector3D.Normalize(direction * vDistance + accel);
 
-            var yaw = ClampRPM((float)localAccel.X);
-            var pitch = ClampRPM((float)localAccel.Y);
-
-            // Actuate the gyros
-            gyroControl.SetAxisVelocityRPM(GyroControl.Yaw, yaw);
-            gyroControl.SetAxisVelocityRPM(GyroControl.Pitch, pitch);
+            double yawError, pitchError;
+            seeker.Seek(shipControl, aimPoint, out yawError, out pitchError);
         }
         else
         {
             // Can't really do crap w/o our own velocity
-            gyroControl.Reset();
+            shipControl.GyroControl.Reset();
         }
 
         eventDriver.Schedule(FramesPerRun, Run);
