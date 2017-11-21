530bf51d5ae59b92f2b44adebdc155a3c709917a
ZerothAngel <zerothangel@tyrannyofheaven.org>
2017-11-20 22:03:07 -0800

    missileguidance: Meh, switch to quadratic.

--- weapon/missileguidance.cs
+++ weapon/missileguidance.cs
@@ -6,8 +6,6 @@ public class MissileGuidance : BaseMissileGuidance
 
     private readonly Seeker seeker = new Seeker(1.0 / RunsPerSecond);
 
-    private Vector3D Prediction;
-
     public void Init(ZACommons commons, EventDriver eventDriver)
     {
         var shipControl = (ShipControlCommons)commons;
@@ -15,8 +13,6 @@ public class MissileGuidance : BaseMissileGuidance
         seeker.Init(shipControl,
                     shipUp: shipControl.ShipUp,
                     shipForward: shipControl.ShipForward);
-
-        Prediction = Target;
 
         eventDriver.Schedule(0, Run);
         eventDriver.Schedule(FULL_BURN_DELAY, FullBurn);
@@ -37,6 +33,58 @@ public class MissileGuidance : BaseMissileGuidance
         thrustControl.Enable(Base6Directions.Direction.Right, false);
     }
 
+    private int QuadraticSolver(double a, double b, double c, out double s1,
+                                out double s2)
+    {
+        s1 = default(double);
+        s2 = default(double);
+
+        if (a == 0.0)
+        {
+            // Actually linear
+            if (b != 0.0)
+            {
+                s1 = -c / b;
+                return 1;
+            }
+            else
+            {
+                // Division by zero...
+                return 0;
+            }
+        }
+        else
+        {
+            // Discriminant
+            var disc = b * b - 4.0 * a * c;
+            if (disc < 0.0)
+            {
+                return 0; // Imaginary
+            }
+            else if (disc == 0.0)
+            {
+                // Single solution
+                s1 = -0.5 * b / a;
+                return 1;
+            }
+            else
+            {
+                // Two solutions
+                var q = (b > 0.0) ? (-0.5 * (b + Math.Sqrt(disc))) : (-0.5 * (b - Math.Sqrt(disc)));
+                s1 = q / a;
+                s2 = c / q;
+                if (s1 > s2)
+                {
+                    // Swap so s1 <= s2
+                    var tmp = s1;
+                    s1 = s2;
+                    s2 = tmp;
+                }
+                return 2;
+            }
+        }
+    }
+
     public void Run(ZACommons commons, EventDriver eventDriver)
     {
         var shipControl = (ShipControlCommons)commons;
@@ -48,26 +96,33 @@ public class MissileGuidance : BaseMissileGuidance
             var delta = eventDriver.TimeSinceStart - LastTargetUpdate;
             var targetGuess = Target + TargetVelocity * delta.TotalSeconds;
 
-            var missileSpeed = ((Vector3D)velocity).Length();
+            // Solve for intersection of expanding sphere + moving object
+            var offset = targetGuess - shipControl.ReferencePoint;
+            var a = Vector3D.Dot(TargetVelocity, TargetVelocity) - Vector3D.Dot((Vector3D)velocity, (Vector3D)velocity);
+            var b = 2.0 * Vector3D.Dot(offset, TargetVelocity);
+            var c = Vector3D.Dot(offset, offset);
 
-            // Get relative positions & distances
-            var relativePosition = targetGuess - shipControl.ReferencePoint;
-            var targetDistance = relativePosition.Length();
-            var predRelativePosition = Prediction - shipControl.ReferencePoint;
-            var predDistance = predRelativePosition.Length();
-            // Detetermine closing speed by projection
-            var closingSpeed = Vector3D.Dot((Vector3D)velocity, predRelativePosition / predDistance);
-            // Clamp to lower bound 1/3rd of current speed
-            closingSpeed = Math.Max(closingSpeed, missileSpeed / 3.0);
-            // Determine time to target using shortest distance
-            var timeToTarget = Math.Min(targetDistance, predDistance) / closingSpeed;
-            // Aim at point where the target will be after timeToTarget secs
-            Prediction = targetGuess + TargetVelocity * timeToTarget;
+            double interceptTime = 20.0;
+
+            double s1, s2;
+            int solutions = QuadraticSolver(a, b, c, out s1, out s2);
+            // Pick smallest positive intercept time
+            if (solutions == 1)
+            {
+                if (s1 > 0.0) interceptTime = s1;
+            }
+            else if (solutions == 2)
+            {
+                if (s1 > 0.0) interceptTime = s1;
+                else if (s2 > 0.0) interceptTime = s2;
+            }
+
+            var prediction = targetGuess + TargetVelocity * interceptTime;
 
             // Determine relative vector to aim point
-            var targetVector = Prediction - shipControl.ReferencePoint;
+            var targetVector = prediction - shipControl.ReferencePoint;
             // Project onto our velocity
-            var velocityNorm = (Vector3D)velocity / missileSpeed;
+            var velocityNorm = Vector3D.Normalize((Vector3D)velocity);
             var forwardProj = velocityNorm * Vector3D.Dot(targetVector, velocityNorm);
             // Use scaled rejection for oversteer
             var forwardRej = (targetVector - forwardProj) * OVERSTEER_FACTOR;
