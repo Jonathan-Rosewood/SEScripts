24eeedab150b70edc146166eaddaf52e4328a969
ZerothAngel <zerothangel@tyrannyofheaven.org>
2017-12-09 14:39:53 -0800

    losminer: Default perturb amplitude to 5m to account for new GyroControl module.

--- utility/losminer-header.cs
+++ utility/losminer-header.cs
@@ -1,5 +1,5 @@
 // LOSMiner
 const double TARGET_MINING_SPEED = 1.0; // In meters per second
 const double MINING_PERTURB_SCALE = 20.0;
-const double MINING_PERTURB_AMPLITUDE = 1.0; // In meters (remember target point is 200m away though)
+const double MINING_PERTURB_AMPLITUDE = 5.0; // In meters (remember target point is 200m away though)
 const float MINING_ROLL_RPM = 0.0f; // In RPM, can be 0.0f
